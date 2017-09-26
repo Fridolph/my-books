34c21cda8eb9e3bb57a399603d28a5c50e4a4a91
fridolph <249121486@qq.com>
2017-09-26 17:35:02 +0800

    链表相关~

--- 学习JavaScript数据结构与算法/第4章 队列/dequence.js
+++ 学习JavaScript数据结构与算法/第4章 队列/dequence.js
@@ -1,36 +1,44 @@
 // 创建队列
-class Queue {
-  constructor() {
-    this.items = [];
+let Queue = (function() {
+  const items = new WeakMap();
+  class Queue {
+    constructor() {
+      items.set(this, []);
+    }
+  
+    // 向队列添加元素
+    enqueue(elem) {
+      items.push(elem);
+    }
+  
+    // 向队列移除元素
+    dequeue() {
+      return items.shift();
+    }
+  
+    // 查看队列头元素
+    front() {
+      console.log(items[0])
+      return items[0];  
+    }
+  
+    // 检查队列是否为空
+    isEmpty() {
+      console.log(`是否为空：${items.length === 0}`)
+      return items.length === 0;
+    }
+  
+    size() {
+      console.log(`队列长度为：${items.length}`);
+      return items.length;
+    }
+  
+    // 打印队列元素
+    print() {
+      console.log(items.valueOf());
+    }
   }
-
-  // 向队列添加元素
-  enqueue(elem) {
-    this.items.push(elem);
-  }
-
-  // 向队列移除元素
-  dequeue() {
-    return this.items.shift();
-  }
-
-  // 查看队列头元素
-  front() {
-    console.log(this.items[0])
-    return this.items[0];  
-  }
-
-  // 检查队列是否为空
-  isEmpty() {
-    console.log(this.items.length === 0)
-    return this.items.length === 0;
-  }
-
-  // 打印队列元素
-  print() {
-    console.log(this.items.valueOf());
-  }
-}
+})();
 
 // 使用Queue类
 let queue = new Queue();
