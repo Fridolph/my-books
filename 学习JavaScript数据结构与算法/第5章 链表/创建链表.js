e0328210408e63f15784dd3511912f225f5d8b0f
fridolph <249121486@qq.com>
2017-09-27 14:33:06 +0800

    创建链表

--- 学习JavaScript数据结构与算法/第5章 链表/创建链表.js
+++ 学习JavaScript数据结构与算法/第5章 链表/创建链表.js
@@ -4,7 +4,7 @@
 function LinkedList() {
   // 其需要一个Node辅助类，Node类表示要加入链表的项
   // 它包含一个element属性，即要添加到链表的值，以及一个next属性，即指向链表中下一个节点项的指针
-  let Node = function(element) {
+  let Node = function (element) {
     this.element = element;
     this.next = null;
   }
@@ -14,9 +14,9 @@ function LinkedList() {
   let head = null;
 
   // 向链表尾部添加一个新的项
-  this.append = function(element) {
+  this.append = function (element) {
     let node = new Node(element),
-        current;
+      current;
 
     // 链表中第一个节点
     if (head === null) {
@@ -37,16 +37,44 @@ function LinkedList() {
   }
 
   // 向链表的特定位置插入一个新的项
-  this.insert = function(position, element) {}
+  this.insert = function (position, element) {
+    // 1检查越界值
+    if (position >= 0 && position <= length) {
+      let node = new Node(element),
+        current = head,
+        previous,
+        index = 0;
+
+      // 在第一个位置上添加
+      if (position === 0) {
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          // 3
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;  // 4
+        previous.next = node; // 5
+      }
+      // 更新列表的长度
+      length++;
+
+      return true;
+    } else {
+      return false; // 6
+    }
+  }
 
   // 从链表的特定位置移除一项
-  this.removeAt = function(position) {
+  this.removeAt = function (position) {
     // 1检查越界值
     if (position > -1 && position < length) {
       let current = head, // 2
-          previous,  // 3
-          index = 0; // 4
-      
+        previous, // 3
+        index = 0; // 4
+
       // 5移除第一项 
       if (position === 0) {
         head = current.next;
@@ -67,17 +95,17 @@ function LinkedList() {
   }
 
   // 从链表中移除一项
-  this.remove = function(element) {}
+  this.remove = function (element) {}
 
   // 返回元素在链表的索引，若没有则返回-1
-  this.indexOf = function(element) {}
+  this.indexOf = function (element) {}
 
   // 若链表中不包含任何元素返回true，反之false
-  this.isEmpty = function() {}
+  this.isEmpty = function () {}
 
   // 返回链表包含的元素个数
-  this.size = function() {}
-  this.getHead = function() {}
-  this.toString = function() {}
-  this.print = function() {}
+  this.size = function () {}
+  this.getHead = function () {}
+  this.toString = function () {}
+  this.print = function () {}
 }
