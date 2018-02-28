b8a09d5acf5bdb4a2aaac73c3c7b973868db3036
fridolph <249121486@qq.com>
2018-02-28 11:29:21 +0800

    种子模块

--- JavaScript框架设计/第1章 种子模块/makeArray.js
+++ JavaScript框架设计/第1章 种子模块/makeArray.js
@@ -1,7 +1,7 @@
 /**
- *c jQuery makeArray方法
- ?c jQuery对象是用来存储与处理dom元素的，它主要依赖于setArray方法来设置和维护长度与索引
- ?c 而setArray的参数要求是一个数组。该方法保证就算没有参数也要返回一个空数组
+ ** jQuery makeArray方法
+ *? jQuery对象是用来存储与处理dom元素的，它主要依赖于setArray方法来设置和维护长度与索引
+ *? 而setArray的参数要求是一个数组。该方法保证就算没有参数也要返回一个空数组
  */
 
 var makeArray = function(array) {
